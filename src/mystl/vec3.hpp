3b816d629f3233f60a1bcde9a4a4e7fd5948413d
chunleili <li_cl@foxmail.com>
2022-12-29 11:38:00 +0800

    增加了vec3一系列运算符重载

--- src/mystl/vec3.hpp
+++ src/mystl/vec3.hpp
@@ -9,7 +9,7 @@
 
 /**
  * @brief 进行数学运算的vec3类。支持加、减、点乘、叉乘、norm等运算。
- * 
+ *
  */
 namespace mystl
 {
@@ -17,58 +17,267 @@ namespace mystl
 class vec3
 {
 public:
-    float x, y, z;  // 分量
+    float x, y, z; // 分量
 
-    vec3() = default; // 默认构造函数
-    vec3(const vec3 &rhs) = default;  // 默认拷贝构造函数
-    vec3 &operator=(const vec3 &rhs) = default;   // 默认拷贝赋值运算符
-    vec3(float val): x(val), y(val), z(val){};  // 用一个值初始化所有分量
-    vec3(float x_, float y_, float z_): x(x_), y(y_), z(z_){};    // 用三个值初始化所有分量
+    vec3() = default;                                           // 默认构造函数
+    vec3(const vec3 &rhs) = default;                            // 默认拷贝构造函数
+    vec3 &operator=(const vec3 &rhs) = default;                 // 默认拷贝赋值运算符
+    vec3(float val) : x(val), y(val), z(val){};                 // 用一个值初始化所有分量
+    vec3(float x_, float y_, float z_) : x(x_), y(y_), z(z_){}; // 用三个值初始化所有分量
 
-    bool operator==(const vec3 &rhs) const;  // 判断相等
-    bool operator!=(const vec3 &rhs) const;  // 判断不等
-    const vec3 & operator+=(const vec3 &rhs); //加等
-    const vec3 & operator-=(const vec3 &rhs); //减等
-    vec3 operator+(const vec3 &rhs) const; //加法
-    vec3 operator-(const vec3 &rhs) const; //减法
-    vec3 operator*(const float rhs) const; //数乘
-    vec3 operator/(const float rhs) const; //数除
-    const vec3 & operator*=(const float rhs); //数乘等
-    const vec3 & operator/=(const float rhs); //数除等
-    float operator*(const vec3 &rhs) const; //点乘
-    vec3 operator^(const vec3 &rhs) const; //叉乘
-    float dot(const vec3 &rhs) const; //点乘
-    vec3 cross(const vec3 &rhs) const; //叉乘
-    float &operator[](int i); //下标访问
-    const float &operator[](int i) const; //下标访问
+    /* -------------------------------------------------------------------------- */
+    /*                                    输入输出                                 */
+    /* -------------------------------------------------------------------------- */
+    friend std::ostream &operator<<(std::ostream &os, const vec3 &v)
+    {
+        os << v.x << " " << v.y << " " << v.z;
+        return os;
+    };
 
-    //模长
-    float norm() const
+    void show() const
+    {
+        std::cout << x << " " << y << " " << z << std::endl;
+    };
+
+
+    /* -------------------------------------------------------------------------- */
+    /*                                  与模长有关的运算                            */
+    /* -------------------------------------------------------------------------- */
+    float norm() const // 模长
     {
         return std::sqrt(x * x + y * y + z * z);
-    }; 
-    float magnitude() const{return this->norm();}; //模长
-    float length() const{return this->norm();}; //模长
-    float normSqr() const   //模长的平方
+    };
+    float magnitude() const // 模长
+    {
+        return this->norm();
+    }
+    float length() const // 模长
+    {
+        return this->norm();
+    }
+    float normSqr() const // 模长的平方
     {
         return (x * x + y * y + z * z);
-    }; 
-    float lengthSqr() const{return this->normSqr();}; //模长的平方
-    const vec3 & normalize()//单位化
+    }
+    float lengthSqr() const // 模长的平方
+    { 
+        return this->normSqr(); 
+    }
+    const vec3 &normalize() // 单位化
     {
         float n = this->norm();
         x /= n;
         y /= n;
         z /= n;
         return *this;
-    }; 
+    };
 
-    //输出
-    friend std::ostream &operator<<(std::ostream &os, const vec3 &v)
+
+    // friend float norm(const vec3 &v) // 模长
+    // {
+    //     return v.norm();
+    // };
+    // friend float length(const vec3 &v) // 模长
+    // {
+    //     return v.norm();
+    // };
+    // friend vec3 normalize(const vec3 &v) // 单位化
+    // {
+    //     float n = v.norm();
+    //     return vec3(v.x / n, v.y / n, v.z / n);
+    // };
+
+
+    /* -------------------------------------------------------------------------- */
+    /*                              与vec3加减乘除运算符                           */
+    /* -------------------------------------------------------------------------- */
+    vec3 operator+(const vec3 &rhs) const    // 加法
     {
-        os << v.x << " " << v.y << " " << v.z;
-        return os;
-    }; 
+        return vec3(this->x + rhs.x, this->y + rhs.y, this->z + rhs.z);
+    }
+    vec3 operator-(const vec3 &rhs) const // 减法
+    {
+        return vec3(this->x - rhs.x, this->y - rhs.y, this->z - rhs.z);
+    }
+    vec3 operator+(const float rhs) const // 数加
+    {
+        return vec3(this->x + rhs, this->y + rhs, this->z + rhs);
+    }
+    vec3 operator-(const float rhs) const // 数减
+    {
+        return vec3(this->x - rhs, this->y - rhs, this->z - rhs);
+    }
+    vec3 operator*(const float rhs) const // 数乘
+    {
+        return vec3(this->x * rhs, this->y * rhs, this->z * rhs);
+    }
+    vec3 operator/(const float rhs) const // 数除
+    {
+        return vec3(this->x / rhs, this->y / rhs, this->z / rhs);
+    }
+    
+    friend vec3 operator+(const float lhs, const vec3 &rhs)// 数加(标量在左)
+    {
+        return vec3(lhs + rhs.x, lhs + rhs.y, lhs + rhs.z);
+    }
+    
+    friend vec3 operator-(const float lhs, const vec3 &rhs)// 数减(标量在左)
+    {
+        return vec3(lhs - rhs.x, lhs - rhs.y, lhs - rhs.z);
+    }
+    
+    friend vec3 operator*(const float lhs, const vec3 &rhs)// 数乘(标量在左)
+    {
+        return vec3(lhs * rhs.x, lhs * rhs.y, lhs * rhs.z);
+    }
+    
+    friend vec3 operator/(const float lhs, const vec3 &rhs)// 数除(标量在左)
+    {
+        return vec3(lhs / rhs.x, lhs / rhs.y, lhs / rhs.z);
+    }
+    
+    float operator*(const vec3 &rhs) const // 点乘
+    {
+        return this->x * rhs.x + this->y * rhs.y + this->z * rhs.z;
+    }
+    float dot(const vec3 &rhs) const // 点乘
+    {
+        return this->x * rhs.x + this->y * rhs.y + this->z * rhs.z;
+    }
+    vec3 operator^(const vec3 &rhs) const // 叉乘
+    {
+        return vec3(this->y * rhs.z - this->z * rhs.y, this->z * rhs.x - this->x * rhs.z, this->x * rhs.y - this->y * rhs.x);
+    }
+    vec3 cross(const vec3 &rhs) const // 叉乘
+    {
+        return vec3(this->y * rhs.z - this->z * rhs.y, this->z * rhs.x - this->x * rhs.z, this->x * rhs.y - this->y * rhs.x);
+    }
+    vec3 cwise_mul(const vec3 &rhs) const // 逐个分量相乘
+    {
+        return vec3(this->x * rhs.x, this->y * rhs.y, this->z * rhs.z);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                                  带等号的运算符重载                          */
+    /* -------------------------------------------------------------------------- */
+    const vec3 &operator+=(const vec3 &rhs) // 加等
+    {
+        this->x += rhs.x;
+        this->y += rhs.y;
+        this->z += rhs.z;
+        return *this;
+    }
+    const vec3 &operator-=(const vec3 &rhs) // 减等
+    {
+        this->x -= rhs.x;
+        this->y -= rhs.y;
+        this->z -= rhs.z;
+        return *this;
+    }
+    const vec3 &operator+=(const float rhs) // 数加等
+    {
+        this->x += rhs;
+        this->y += rhs;
+        this->z += rhs;
+        return *this;
+    }
+    const vec3 &operator-=(const float rhs) // 数减等
+    {
+        this->x -= rhs;
+        this->y -= rhs;
+        this->z -= rhs;
+        return *this;
+    }
+    const vec3 &operator*=(const float rhs) // 数乘等
+    {
+        this->x *= rhs;
+        this->y *= rhs;
+        this->z *= rhs;
+        return *this;
+    }
+    const vec3 &operator/=(const float rhs) // 数除等
+    {
+        this->x /= rhs;
+        this->y /= rhs;
+        this->z /= rhs;
+        return *this;
+    }
+
+
+    /* -------------------------------------------------------------------------- */
+    /*                                   关系运算符                                */
+    /* -------------------------------------------------------------------------- */
+    bool operator==(const vec3 &rhs) const  // 判断相等(所有分量相等返回true)
+    {
+        return this->x == rhs.x && this->y == rhs.y && this->z == rhs.z;
+    }
+    bool operator!=(const vec3 &rhs) const  // 判断不等(任何分量不等于都返回true)
+    {
+        return this->x != rhs.x || this->y != rhs.y || this->z != rhs.z;
+    }
+    bool operator>(const vec3 &rhs) const   // 判断大于(norm)
+    {
+        return this->norm() > rhs.norm();
+    }
+    bool operator<(const vec3 &rhs) const   // 判断小于(norm)
+    {
+        return this->norm() < rhs.norm();
+    }
+    bool operator>=(const vec3 &rhs) const  // 判断大于等于(norm)
+    {
+        return this->norm() >= rhs.norm();
+    }
+    bool operator<=(const vec3 &rhs) const  // 判断小于等于(norm)
+    {
+        return this->norm() <= rhs.norm();
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                                    下标运算符                               */
+    /* -------------------------------------------------------------------------- */
+    float &operator[](int i)             // 下标访问
+    {
+        return (&x)[i];
+    }
+    const float &operator[](int i) const // 下标访问
+    {
+        return (&x)[i];
+    }
+    float & at(int i)     
+    {
+        if (i < 0 || i > 2)
+            throw std::out_of_range("vec3 index out of range");
+        return (&x)[i];
+    }        
+    const float &at(int i) const   
+    {
+        if (i < 0 || i > 2)
+            throw std::out_of_range("vec3 index out of range");
+        return (&x)[i];
+    }          
+
+    /* -------------------------------------------------------------------------- */
+    /*                                    单目运算符                               */
+    /* -------------------------------------------------------------------------- */
+    vec3 operator-() const // 取负
+    {
+        return vec3(-this->x, -this->y, -this->z);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                                     其他                                   */
+    /* -------------------------------------------------------------------------- */
+    float distance(const vec3 &rhs) const // 两点间距离
+    {
+        return sqrt((this->x - rhs.x) * (this->x - rhs.x) + (this->y - rhs.y) * (this->y - rhs.y) + (this->z - rhs.z) * (this->z - rhs.z));
+    }
+
+    friend float distance(const vec3 &lhs, const vec3 &rhs) // 两点间距离
+    {
+        return sqrt((lhs.x - rhs.x) * (lhs.x - rhs.x) + (lhs.y - rhs.y) * (lhs.y - rhs.y) + (lhs.z - rhs.z) * (lhs.z - rhs.z));
+    }
 };
+
+
 
 } // namespace mystl
--- tests/test_vec3.cpp
+++ tests/test_vec3.cpp
@@ -80,17 +80,6 @@ TEST_CASE("vec3 correctly copy", "[vec3]")
     }
 }
 
-
-TEST_CASE("ostream", "[vec3]")
-{
-    SECTION( "ostream" ) 
-    {
-        mystl::vec3 v(1,2,3);
-        
-        std::cout <<"vec3 is: "<< v << std::endl;
-    }
-}
-
 TEST_CASE("work with vector", "[vec3]")
 {
     SECTION( "std vector" ) 
@@ -106,6 +95,18 @@ TEST_CASE("work with vector", "[vec3]")
             std::cout << v << std::endl;
     }
 }
+
+TEST_CASE("input output", "[vec3]")
+{
+    SECTION( "ostream" ) 
+    {
+        mystl::vec3 v(1,2,3);
+        std::cout <<"vec3 is: "<< v << std::endl;
+    }
+
+}
+
+
 
 
 TEST_CASE("norm", "[vec3]")
@@ -151,6 +152,342 @@ TEST_CASE("norm", "[vec3]")
         REQUIRE(v.normalize().x==Approx(x_normalized).epsilon(0.0001));
         REQUIRE(v.normalize().y==Approx(y_normalized).epsilon(0.0001));
         REQUIRE(v.normalize().z==Approx(z_normalized).epsilon(0.0001));
+    }
+
+
+
+    // SECTION("norm (friend)")
+    // {
+    //     mystl::vec3 v(1,2,3);
+    //     REQUIRE(mystl::norm(v)==Approx(std::sqrt(14)).epsilon(0.0001));
+    // }
+
+    // SECTION("length (friend)")
+    // {
+    //     mystl::vec3 v(1,2,3);
+    //     REQUIRE(mystl::length(v)==Approx(std::sqrt(14)).epsilon(0.0001));
+    // }
+
+    // SECTION("normalize (friend)")
+    // {
+    //     mystl::vec3 v(1,2,3);
+    //     float length = std::sqrt(14);
+    //     float x_normalized = 1.0/length;
+    //     float y_normalized = 2.0/length;
+    //     float z_normalized = 3.0/length;
+    //     mystl::vec3 v2;
+    //     v2 = mystl::normalize(v);
+    //     REQUIRE(v2.x == Approx(x_normalized).epsilon(0.0001));
+    //     REQUIRE(v2.y == Approx(y_normalized).epsilon(0.0001));
+    //     REQUIRE(v2.z == Approx(z_normalized).epsilon(0.0001));
+    // }
+
+}
+
+TEST_CASE("operator", "[vec3]")
+{
+    SECTION("+")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,3);
+        mystl::vec3 v3 = v + v2;
+        REQUIRE(v3.x==2);
+        REQUIRE(v3.y==4);
+        REQUIRE(v3.z==6);
+    }
+    SECTION("-")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,3);
+        mystl::vec3 v3 = v - v2;
+        REQUIRE(v3.x==0);
+        REQUIRE(v3.y==0);
+        REQUIRE(v3.z==0);
+    }
+    SECTION("+(with scalar)")
+    {
+        mystl::vec3 v(1,2,3);
+        float scalar = 2;
+        mystl::vec3 v2 = v + scalar;
+        REQUIRE(v2.x==3);
+        REQUIRE(v2.y==4);
+        REQUIRE(v2.z==5);
+    }
+    SECTION("-(with scalar)")
+    {
+        mystl::vec3 v(1,2,3);
+        float scalar = 2;
+        mystl::vec3 v2 = v - scalar;
+        REQUIRE(v2.x==-1);
+        REQUIRE(v2.y==0);
+        REQUIRE(v2.z==1);
+    }
+    SECTION("*(with scalar)")
+    {
+        mystl::vec3 v(1,2,3);
+        float scalar = 2;
+        mystl::vec3 v2 = v * scalar;
+        REQUIRE(v2.x==2);
+        REQUIRE(v2.y==4);
+        REQUIRE(v2.z==6);
+    }
+    SECTION("/(with scalar)")
+    {
+        mystl::vec3 v(1,2,3);
+        float scalar = 2;
+        mystl::vec3 v2 = v / scalar;
+        REQUIRE(v2.x==0.5);
+        REQUIRE(v2.y==1);
+        REQUIRE(v2.z==1.5);
+    }
+    SECTION("+(scalar on the left)")
+    {
+        mystl::vec3 v(1,2,3);
+        float scalar = 2;
+        mystl::vec3 v2 = scalar + v;
+        REQUIRE(v2.x==3);
+        REQUIRE(v2.y==4);
+        REQUIRE(v2.z==5);
+    }
+    SECTION("-(scalar on the left)")
+    {
+        mystl::vec3 v(1,2,3);
+        float scalar = 2;
+        mystl::vec3 v2 = scalar - v;
+        REQUIRE(v2.x==1);
+        REQUIRE(v2.y==0);
+        REQUIRE(v2.z==-1);
+    }
+    SECTION("*(scalar on the left)")
+    {
+        mystl::vec3 v(1,2,3);
+        float scalar = 2;
+        mystl::vec3 v2 = scalar * v;
+        REQUIRE(v2.x==2);
+        REQUIRE(v2.y==4);
+        REQUIRE(v2.z==6);
+    }
+    SECTION("/(scalar on the left)")
+    {
+        mystl::vec3 v(1,2,3);
+        float scalar = 2;
+        mystl::vec3 v2 = scalar / v;
+        REQUIRE(v2.x==2);
+        REQUIRE(v2.y==1);
+        REQUIRE(v2.z==Approx(2.0/3.0).epsilon(0.0001));
+    }
+
+    SECTION("*")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,3);
+        float dot = v * v2;
+        REQUIRE(dot == 14);
+    }
+    SECTION("dot")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,3);
+        float dot = v.dot(v2);
+        REQUIRE(dot==14);
+    }
+    SECTION("^")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,3);
+        mystl::vec3 v3 = v^v2;
+        REQUIRE(v3.x==0);
+        REQUIRE(v3.y==0);
+        REQUIRE(v3.z==0);
+    }
+    SECTION("cross")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,3);
+        mystl::vec3 v3 = v.cross(v2);
+        REQUIRE(v3.x==0);
+        REQUIRE(v3.y==0);
+        REQUIRE(v3.z==0);
+    }
+    SECTION("cwise_mul")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,3);
+        mystl::vec3 v3 = v.cwise_mul(v2);
+        REQUIRE(v3.x==1);
+        REQUIRE(v3.y==4);
+        REQUIRE(v3.z==9);
+    }
+
+
+
+}
+
+TEST_CASE("operator += -= *= /=","[vec3]")
+{
+    SECTION("+= (with vec3)")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,3);
+        v += v2;
+        REQUIRE(v.x==2);
+        REQUIRE(v.y==4);
+        REQUIRE(v.z==6);
+    }
+
+    SECTION("-= (with vec3)")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,3);
+        v += v2;
+        REQUIRE(v.x==2);
+        REQUIRE(v.y==4);
+        REQUIRE(v.z==6);
+    }
+
+    SECTION("+= (with scalar)")
+    {
+        mystl::vec3 v(1,2,3);
+        float scalar = 2;
+        v += scalar;
+        REQUIRE(v.x==3);
+        REQUIRE(v.y==4);
+        REQUIRE(v.z==5);
+    }
+
+    SECTION("-= (with scalar)")
+    {
+        mystl::vec3 v(1,2,3);
+        float scalar = 2;
+        v -= scalar;
+        REQUIRE(v.x==-1);
+        REQUIRE(v.y==0);
+        REQUIRE(v.z==1);
+    }
+
+    SECTION("*= (with scalar)")
+    {
+        mystl::vec3 v(1,2,3);
+        float scalar = 2;
+        v *= scalar;
+        REQUIRE(v.x==2);
+        REQUIRE(v.y==4);
+        REQUIRE(v.z==6);
+    }
+
+    SECTION("/= (with scalar)")
+    {
+        mystl::vec3 v(1,2,3);
+        float scalar = 2;
+        v /= scalar;
+        REQUIRE(v.x==0.5);
+        REQUIRE(v.y==1);
+        REQUIRE(v.z==1.5);
+    }
+}
+
+TEST_CASE("relation operator","[vec3]")
+{
+    SECTION("==")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,3);
+        REQUIRE(v==v2);
+    }
+    SECTION("!=")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,4);
+        REQUIRE(v!=v2);
+    }
+    SECTION("< (norm)")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,4);
+        REQUIRE(v<v2);
+    }
+    SECTION("<= (norm)")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,4);
+        REQUIRE(v<=v2);
+    }
+    SECTION("> (norm)")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,4);
+        REQUIRE(v2>v);
+    }
+    SECTION(">= (norm)")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,4);
+        REQUIRE(v2>=v);
+    }
+}
+
+TEST_CASE("operator []","[vec3]")
+{
+    SECTION("operator [] (const)")
+    {
+        mystl::vec3 v(1,2,3);
+        REQUIRE(v[0]==1);
+        REQUIRE(v[1]==2);
+        REQUIRE(v[2]==3);
+    }
+
+    SECTION("operator [] (non-const)")
+    {
+        mystl::vec3 v(1,2,3);
+        v[0] = 2;
+        v[1] = 3;
+        v[2] = 4;
+        REQUIRE(v[0]==2);
+        REQUIRE(v[1]==3);
+        REQUIRE(v[2]==4);
+    }
+
+    SECTION("at ")
+    {
+        mystl::vec3 v(1,2,3);
+        REQUIRE(v.at(0)==1);
+        REQUIRE(v.at(1)==2);
+        REQUIRE(v.at(2)==3);
+    }
+
+    SECTION("at (out of range)")
+    {
+        mystl::vec3 v(1,2,3);
+        REQUIRE_THROWS_AS(v.at(3),std::out_of_range);
+    }
+}
+
+TEST_CASE("unary operator","[vec3]")
+{
+    SECTION("unary -")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2 = -v;
+        REQUIRE(v2.x==-1);
+        REQUIRE(v2.y==-2);
+        REQUIRE(v2.z==-3);
+    }
+}
+
+
+TEST_CASE("other","vec3")
+{
+    SECTION("distance")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,4);
+        REQUIRE(v.distance(v2)==1);
+    }
+    SECTION("distance (friend)")
+    {
+        mystl::vec3 v(1,2,3);
+        mystl::vec3 v2(1,2,4);
+        REQUIRE(distance(v,v2)==1);
     }
 
 }
